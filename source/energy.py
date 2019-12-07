09f2c7f96492cf08a70c3776da45778244ebb8b7
ss-ghule <ss.ghule@ncl.res.in>
2019-12-07 21:03:28 +0530

    create translation.py update energy.py, rotation.py

--- source/energy.py
+++ source/energy.py
@@ -4,60 +4,165 @@ sys.path.extend(['..'])
 
 from lib.io_chem import io
 from lib.basic_operations import vector,physics
-from bin import config
+import config
 
-def getNetRKE(file,start_frame_no,end_frame_no,part='ring',step_size=1,method='atom_axis',atom_list=[]):
+def getAvgRKE(file,start_frame_no,end_frame_no,step_size,part1='ring',part2='track',type='abs',method='atomic_r_t',part1_atom_list=[],part2_atom_list=[]):
   assert end_frame_no>=start_frame_no,'Invalid Frame Numbers'
   prev_frame_no=start_frame_no
   prev_frame_cords=io.readFileMd(file,prev_frame_no,frame_no_pos=2)
   for curr_frame_no in range(start_frame_no+1,end_frame_no+1,step_size):
     curr_frame_cords=io.readFileMd(file,curr_frame_no)
-    part_RKE=_getRKE(prev_frame_cords,curr_frame_cords,part=part,method=method,atom_list=[])
-
-    net_part_RKE+=part_RKE
+    RKE=_getRKE(prev_frame_cords,curr_frame_cords,part1=part1,part2=part2,type=type,method=method,part1_atom_list=part1_atom_list,part2_atom_list=part2_atom_list)
+    net_RKE+=RKE
     prev_frame_no=curr_frame_no
     prev_frame_cords=curr_frame_cords.copy()
-  return net_part_RKE
+  avg_RKE=net_RKE/len(range(start_frame_no,end_frame_no+1,step_size)
+  return avg_RKE
 
-def getRKE(file,frame1_no,frame2_no,part='ring',method='atom_axis',atom_list=[]):
+def getRKE(file,frame1_no,frame2_no,part1='ring',part2='track',type='abs',method='r+t',part1_atom_list=[],part2_atom_list=[]):
   assert frame2_no>=frame1_no,'Invalid Frame Numbers'
   frame1_cords=io.readFileMd(file,frame1_cords,frame_no_pos=2)
   frame2_cords=io.readFileMd(file,frame2_cords,frame_no_pos=2)
-  return _getRKE(frame1_cords,frame2_cords,part=part,method=method,atom_list=atom_list)
+  return _getRKE(frame1_cords,frame2_cords,part1=part1,part2=part2,type=type,method=method,part1_atom_list=part1_atom_list,part2_atom_list=part2_atom_list)
 
-def _getRKE(frame1_cords,frame2_cords,part='ring',method='atom_axis',atom_list=[]):
-  part_RKE=0
-  if part=='ring':
-    _atom_list=range(config.ring_start_atom_no,config.ring_end_atom_no+1)
-  elif part=='track':
-    _atom_list=range(config.track_start_atom_no,config.track_end_atom_no+1)
-  elif part=='whole':
-    _atom_list=io.readFileMd(file,0,'atoms')
+def _getRKE(frame1_cords,frame2_cords,part1='ring',part2='track',type='abs',method='r+t',part1_atom_list=[],part2_atom_list=[]):
+  RKE=0 
+ 
+  if method=='energy_atomic_angle':
+    RKE=energy_atomic_angle(frame1_cords,frame2_cords,part1=part1,part2=part2,type=type,part1_atom_list=part1_atom_list,part2_atom_list=part2_atom_list)
+  elif method=='energy_atomc_r_t':
+    part1_rotation=atomic_r_t(frame1_cords,frame2_cords,part1=part1,part2=part2,type=type,part1_atom_list=part1_atom_list,part2_atom_list=part2_atom_list)
   else:
-    assert len(atom_list)!=0,'atoms_list should not be empty'
-    _atom_list=atom_list
-       
-  for atom_no in _atom_list:
-    frame1_atom_cords=frame1_cords[frame1_cords['atom_no']==atom_no][['x','y','z']].values
-    frame2_atom_cords=frame2_cords[frame2_cords['atom_no']==atom_no][['x','y','z']].values
-    atom_type=frame1_cords[frame1_cords['atom_no']==atom_noc]['atom'].values[0]
-    if method=='atom_axis':
-      atom_RKE=getAtomRKE(frame1_atom_cords,frame2_atom_cords,atom_type)
-    elif method=='com_axis':
-      pass
-    elif method=='r+t':
-      pass
-    elif method=='t+r':
-      pass
-    else:
-      print('Please specify valid method')
-    part_RKE+=atom_RKE
-  return part_RKE
+    print('Please give an appropriate method')
+  return RKE
+   
+def energy_atomic_angle(frame1_cords,frame2_cords,part1='ring',part2='track',type='abs',part1_atom_list=[],part2_atom_list=[]):
+ 
+  if part1=='ring':
+    _part1_atom_list=range(config.ring_start_atom_no,config.ring_end_atom_no+1)
+  elif part1=='track':
+    _part1_atom_list=range(config.track_start_atom_no,config.track_end_atom_no+1)
+  else:
+    assert len(part1_atom_list)!=0,'atoms_list should not be empty'
+    _part1_atom_list=part1_atom_list
 
-def getAtomRKE(atom_cords1,atom_cords2,atom_type):
-  theta=vector.getAngleR(atom_cords1,atom_cords2)
-  omega=theta/(config.simulation_time_step*config.femto)
-  RKE=0.5*physics.getMI(atom_cords,atom_type)*pow(omega,2)
+  if type=='abs':    
+    for atom_no in _part1_atom_list:
+      frame1_atom_cords=frame1_cords[frame1_cords['atom_no']==atom_no][['x','y','z']].values
+      frame2_atom_cords=frame2_cords[frame2_cords['atom_no']==atom_no][['x','y','z']].values
+      atom_type=frame1_cords[frame1_cords['atom_no']==atom_no]['atom'].values[0]
+      theta=vector.getAngleR(atom_cords1,atom_cords2)
+      omega=theta/(config.simulation_time_step*config.femto)
+      atom_RKE=0.5*physics.getMI(atom_cords,atom_type)*pow(omega,2)
+      RKE+=atom_RKE
+
+  elif type=='rel'
+    pass
+
   return RKE
 
+def energy_atomic_r_t(frame1_cords,frame2_cords,part1='ring',part2='track',type='abs',part1_atom_list=[],part2_atom_list=[]):
+  
+  if part1=='ring':
+    _part1_atom_list=range(config.ring_start_atom_no,config.ring_end_atom_no+1)
+  elif part1=='track':
+    _part1_atom_list=range(config.track_start_atom_no,config.track_end_atom_no+1)
+  else:
+    assert len(part1_atom_list)!=0,'atoms_list should not be empty'
+    _part1_atom_list=part1_atom_list
+
+  if type=='abs':
+    theta=rotation.atomic_r_t(frame1_cords,frame2_cords,part=part1,part1_atom_list=atom_list)
+    omega=theta/(config.simulation_time_step*config.femto)
+    MI=0
+    if config.axis=='x':
+      ax=[1,0,0]
+    elif config.axis='y':
+      ax=[0,1,0]
+    elif config.axis='z':
+      ax=[0,0,1]
+    mass_list=[]
+    distance_list=[]
+    for atom_no in part1_atom_list:
+      atom_cords=frame1_cords[frame1_cords['atom_no']==atom_no][['x','y','z']].values
+      atom_type=frame1_cords[frame1_cords['atom_no']==atom_no]['atom'].values[0]
+      distance=vector.get_dist_pt_line(point,ax)
+      mass=atomic_mass.atomic_mass_dict[atom_type]
+      mass_list.append(mass)
+      distance_list.append(distance)
+    MI=physics._getMI(mass_list,distance_list)
+    RKE=0.5*MI*pow(omega,2)
+
+  elif type=='rel'
+    if part2=='ring':
+      _part2_atom_list=range(config.ring_start_atom_no,config.ring_end_atom_no+1)
+    elif part2=='track':
+      _part2_atom_list=range(config.track_start_atom_no,config.track_end_atom_no+1)
+    else:
+      assert len(part2_atom_list)!=0,'atoms_list should not be empty'
+      _part2_atom_list=part2_atom_list
+
+    part1_theta=rotation.atomic_r_t(frame1_cords,frame2_cords,part=part1,atom_list=part1_atom_list)
+    part2_theta=rotation.atomic_r_t(frame1_cords,frame2_cords,part=part2,atom_list=part2_atom_list)
+    theta=part1_theta-part2_theta
+    omega=theta/(config.simulation_time_step*config.femto)
+    MI=0
+    if config.axis=='x':
+      ax=[1,0,0]
+    elif config.axis='y':
+      ax=[0,1,0]
+    elif config.axis='z':
+      ax=[0,0,1]
+    mass_list=[]
+    distance_list=[]
+    for atom_no in part1_atom_list:
+      atom_cords=frame1_cords[frame1_cords['atom_no']==atom_no][['x','y','z']].values
+      atom_type=frame1_cords[frame1_cords['atom_no']==atom_no]['atom'].values[0]
+      distance=vector.get_dist_pt_line(point,ax)
+      mass=atomic_mass.atomic_mass_dict[atom_type]
+      mass_list.append(mass)
+      distance_list.append(distance)
+    MI=physics._getMI(mass_list,distance_list)
+    RKE=0.5*MI*pow(omega,2)
+  return RKE
+
+#Translational Energy
+def getAvgTKE(file,start_frame_no,end_frame_no,step_size,part1='ring',part2='track',type='abs',method='atomic_r_t',part1_atom_list=[],part2_atom_list=[]):
+  assert end_frame_no>=start_frame_no,'Invalid Frame Numbers'
+  prev_frame_no=start_frame_no
+  prev_frame_cords=io.readFileMd(file,prev_frame_no,frame_no_pos=2)
+  for curr_frame_no in range(start_frame_no+1,end_frame_no+1,step_size):
+    curr_frame_cords=io.readFileMd(file,curr_frame_no)
+    TKE=_getTKE(prev_frame_cords,curr_frame_cords,part1=part1,part2=part2,type=type,method=method,part1_atom_list=part1_atom_list,part2_atom_list=part2_atom_list)
+    net_TKE+=TKE
+    prev_frame_no=curr_frame_no
+    prev_frame_cords=curr_frame_cords.copy()
+  avg_TKE=net_TKE/len(range(start_frame_no,end_frame_no+1,step_size)
+  return avg_TKE
+
+def getTKE(file,frame1_no,frame2_no,part1='ring',part2='track',type='abs',method='r+t',part1_atom_list=[],part2_atom_list=[]):
+  assert frame2_no>=frame1_no,'Invalid Frame Numbers'
+  frame1_cords=io.readFileMd(file,frame1_cords,frame_no_pos=2)
+  frame2_cords=io.readFileMd(file,frame2_cords,frame_no_pos=2)
+  return _getTKE(frame1_cords,frame2_cords,part1=part1,part2=part2,type=type,method=method,part1_atom_list=part1_atom_list,part2_atom_list=part2_atom_list)
+
+def _getTKE(frame1_cords,frame2_cords,part1='ring',part2='track',type='abs',method='r+t',part1_atom_list=[],part2_atom_list=[]):
+  RKE=0
+
+  if method=='energy_atomic_angle':
+    RKE=energy_atomic_angle(frame1_cords,frame2_cords,part1=part1,part2=part2,type=type,part1_atom_list=part1_atom_list,part2_atom_list=part2_atom_list)
+  elif method=='energy_atomc_r_t':
+    part1_rotation=atomic_r_t(frame1_cords,frame2_cords,part1=part1,part2=part2,type=type,part1_atom_list=part1_atom_list,part2_atom_list=part2_atom_list)
+  else:
+    print('Please give an appropriate method')
+  return RKE
+
+def energy_atomic_angle(frame1_cords,frame2_cords,part1='ring',part2='track',type='abs',part1_atom_list=[],part2_atom_list=[]):
+
+  if part1=='ring':
+    _part1_atom_list=range(config.ring_start_atom_no,config.ring_end_atom_no+1)
+  elif part1=='track':
+    _part1_atom_list=range(config.track_start_atom_no,config.track_end_atom_no+1)
+  else:
+    assert len(part1_atom_list)!=0,'atoms_list should not be empty'
 
--- source/translation.py
+++ source/translation.py
@@ -8,7 +8,7 @@ from lib.basic_operations import vector
 import config
 
 
-def getNetRotation(file,start_frame_no,end_frame_no,step_size,part1='ring',part2='track',type='abs',method='atomic_r_t',part1_atom_list=[],part2_atom_list=[]):
+def getNetTranslation(file,start_frame_no,end_frame_no,step_size,part1='ring',part2='track',type='abs',method='atomic_r_t',part1_atom_list=[],part2_atom_list=[]):
   assert curr_frame_no>=start_frame_no,'Invalid Frame Numbers'
   prev_frame_no=start_frame_no
   prev_frame_cords=io.readFileMd(file,prev_frame_no,frame_no_pos=2)
@@ -54,7 +54,7 @@ def _getRotation(frame1_cords,frame2_cords,part1='ring',part2='track',type='abs'
       print('Please give an appropriate method')
   return rotation
 
-def atomic_r_t(frame1_cords,frame2_cords,part='ring',atom_list=[]):
+def trans_atomic_r_t(frame1_cords,frame2_cords,part='ring',atom_list=[]):
   if part=='ring':
     _atom_list=range(config.ring_start_atom_no,config.ring_end_atom_no+1)
   elif part=='track':
@@ -69,63 +69,20 @@ def atomic_r_t(frame1_cords,frame2_cords,part='ring',atom_list=[]):
     atom_rotation=getRPYAngles(frame1_atom_cords,frame2_atom_cords)
     #getRPYAngles with new prev_frame_cords
     #translate cords of prev_frame
-    if config.axis=='x':
-      ax=0
-    elif config.axis=='y':
-      ax=1
-    elif config.axis=='z':
-      ax=2
-    net_rotation+=atom_rotation[ax]
-  avg_rotation=net_rotation/len(atom_list)
-  return avg_rotation
+    mag1=vector.getMag(frame1_atom_cords)
+    mag2=vector.getMag(frame2_atom_cords)
+    trans_vec=(mag2-mag1)*vector.getUnitVec(frame2_atom_cords)
+    net_translation+=trans_vec[ax]
+  avg_translation=net_translation/len(atom_list)
+  return avg_translation
 
-def atomic_t_r(frame1_cords,frame2_cords,part='ring',method='r+t',atom_list=[]):
-  pass
-  #translate cords of prev_frame
-  #getRPYAngles with new prev_frame_cords
-
-def getRotMat(ax,theta):
-  R=np.zeros((3,3))
-  s=vector.getUnitVec(ax)
-  t=theta 
-  vv=(1-cos(t))
-  R[0][0]=s[0]*s[0]*vv+cos(t)
-  R[0][1]=s[0]*s[1]*vv-s[2]*sin(t)
-  R[0][2]=s[0]*s[2]*vv+s[1]*sin(t)
-  R[1][0]=s[0]*s[1]*vv+s[2]*sin(t)
-  R[1][1]=s[1]*s[1]*vv+cos(t)
-  R[1][2]=s[1]*s[2]*vv-s[0]*sin(t)
-  R[2][0]=s[0]*s[2]*vv-s[1]*sin(t)
-  R[2][1]=s[1]*s[2]*vv+s[0]*sin(t)
-  R[2][2]=s[2]*s[2]*vv+cos(t)
-  return R
-
-def rotateAlongAxis(cords,ax,theta):
-  new_cords=cords.copy()
-  R=getRotMat(ax,theta)
-  _cords=cords[['x','y','z']].values
-  new_cords[['x','y','z']]=np.matmul(_cords,R.T)
-  return new_cords
-
-def getRPYangles(v1,v2,ax='x'):
-  rpy=np.zeros(3)
-  s=vector.getCrossProduct(v1,v2) 
-  s=vector.getUnitVec(s)
-  theta=vector.getAngleR(v1,v2)
-  R=getRotMat(s,theta)
-  if ax=='x':
-    rpy[1]=asin(-1*R[2][0])
-    rpy[0]=asin(R[2][1]/cos(rpy[1]))
-    rpy[2]=asin(R[1][0]/cos(rpy[1]))
-  elif ax=='y':
-    rpy[2]=asin(-1*R[0][1])
-    rpy[1]=asin(R[0][2]/cos(rpy[2]))
-    rpy[0]=asin(R[2][1]/cos(rpy[2]))
-  elif ax=='z':
-    rpy[0]=asin(-1*R[1][2])
-    rpy[1]=asin(R[0][2]/cos(rpy[0]))
-    rpy[2]=asin(R[1][0]/cos(rpy[0]))
+def trans_com(frame1_cords,frame2_cords,part='ring',atom_list=[]):
+  if part=='ring':
+    _atom_list=range(config.ring_start_atom_no,config.ring_end_atom_no+1)
+  elif part=='track':
+    _atom_list=range(config.track_start_atom_no,config.track_end_atom_no+1)
   else:
-    print('To be implemented')
-  return rpy
+    assert len(atom_list)!=0,'atoms_list should not be empty'
+    _atom_list=atom_list
 
+   
