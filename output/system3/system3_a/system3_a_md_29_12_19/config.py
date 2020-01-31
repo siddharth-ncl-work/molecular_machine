73e11f33ed8469203e5a0e11e3ba0dfb82002fb1
ss-ghule <ss.ghule@ncl.res.in>
2020-01-31 09:59:49 +0530

    code is ready for net_relative_translation data generation

--- output/system3/system3_a/system3_a_md_29_12_19/config.py
+++ output/system3/system3_a/system3_a_md_29_12_19/config.py
@@ -28,7 +28,7 @@ translation_method='trans_com'
 RKE_method='energy_rot_hybrid_1'
 TKE_method='energy_trans_com'
 axis='x'
-show_plot=True
+show_plot=False
 
 ring_atom_no_list=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63]
 
