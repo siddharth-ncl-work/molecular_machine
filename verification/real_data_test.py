f64f683b7060b61d1d4213759590bfda16ebca1f
ss-ghule <ss.ghule@ncl.res.in>
2020-02-01 08:07:57 +0530

    * verificatio is not yet finished * direct translation between first and last frame might be more reasonable than rediculus high net_relative_translation * need to figure out error in net_relative_translation * but in case of rotation directio rotation between first and last frames might be better, need to check this * report direct and net values

--- verification/real_data_test.py
+++ verification/real_data_test.py
@@ -2,7 +2,7 @@ import sys
 sys.path.extend(['.','..'])
 
 import config
-from source import rotation,init
+from source import rotation,translation,init
 
 file_path=config.test_file_path
 init.initConfig(file_path)
@@ -10,6 +10,8 @@ method='rot_part_atomic_r_t_3'
 frame1_no=config.start_frame_no
 frame2_no=config.end_frame_no
 with open(file_path,'r') as file:
-  _rotation=rotation.getNetRotation(file,frame1_no,frame2_no,part1='ring',part2='track',type='relative',method=method)
-print(_rotation)  
-
+  #_rotation=rotation.getNetRotation(file,frame1_no,frame2_no,part1='ring',part2='track',type='relative',method=method)
+  #_rotation=rotation.getRotation(file,frame1_no,frame2_no,part1='ring',part2='track',type='relative',method=method)
+  #print(_rotation)  
+  _translation=translation.getTranslation(file,frame1_no,frame2_no,part1='ring',part2='track',type='relative',method=config.translation_method) 
+  print(_translation)
