ead5ff911abc86e0ddcbaf2b1ccd0161d7680e16
ss-ghule <ss.ghule@ncl.res.in>
2020-02-24 03:57:26 +0530

    * will be finshing data-1.1.0 branch and the returning to this branch

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ input_subsystem_name="system3_a"
 output_parent_dir_path='output'
 
 ring_atom_no=0
-track_atom_no=30
-ref_axis_atom1_no=20
-ref_axis_atom2_no=37
+track_atom_no=153
+ref_axis_atom1_no=75
+ref_axis_atom2_no=98
 
 start_frame_no=40
 end_frame_no=99
@@ -31,13 +31,13 @@ TKE_method='energy_trans_com'
 axis='x'
 show_plot=False
 
-ring_atom_no_list=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+ring_atom_no_list=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63]
 
-track_atom_no_list=[20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37]
+track_atom_no_list=[64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153]
 
 code_dir_path='/home/vanka/siddharth/molecular_machines_project/molecular_machines'
 
 '''
-* due to emergency verification-2.0.0 will be done later if needed
-* now creating data-2.0.0 branch for david(case 2) data generation
+* finishing verification-2.0.0 branch to make sure generated data is valid
+* in data-2.0.0 branch data was generated for case_1
 '''
