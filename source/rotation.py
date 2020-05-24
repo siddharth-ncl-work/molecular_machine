-fix error with the track single axis(x) rotation test -even if track does not translate, its cog/com moves -movement of cog/com gives rise to ratation(if ring does not move) -better strategy is to use axis passing through cog of ring and track -need re-think about rotation axis strategy for only ring and track

source/rotation.py
@@ -16,7 +16,8 @@ def getNetRotation(file,start_frame_no,end_frame_no,step_size=1,part1='ring',par
   assert end_frame_no>=start_frame_no,'Invalid Frame Numbers'
   prev_frame_no=start_frame_no
   prev_frame_cords=io.readFileMd(file,prev_frame_no,frame_no_pos=config.frame_no_pos)
-  for curr_frame_no in tqdm(range(start_frame_no+step_size,end_frame_no+1,step_size)):
+  pbar=tqdm(range(start_frame_no+step_size,end_frame_no+1,step_size))
+  for curr_frame_no in pbar:
     curr_frame_cords=io.readFileMd(file,curr_frame_no,frame_no_pos=config.frame_no_pos)
     rotation=_getRotation(prev_frame_cords,curr_frame_cords,part1=part1,part2=part2,type=type,method=method,part1_atom_list=part1_atom_list,part2_atom_list=part2_atom_list)
     net_rotation+=rotation    
@@ -24,14 +25,13 @@ def getNetRotation(file,start_frame_no,end_frame_no,step_size=1,part1='ring',par
     prev_frame_cords=curr_frame_cords.copy()
     data['frame_no'].append(curr_frame_no)
     data['rotation'].append(rotation)
+    pbar.set_description(f'net_{part1}_{type}_rotation = {round(net_rotation,4)} deg')
   return (net_rotation,data)
 
 def getRotation(file,frame1_no,frame2_no,part1='ring',part2='track',type='absolute',method='rot_atomic_r_t_2',part1_atom_list=[],part2_atom_list=[]):
   assert frame2_no>=frame1_no,'Invalid Frame Numbers'
   frame1_cords=io.readFileMd(file,frame1_no,frame_no_pos=config.frame_no_pos)
   frame2_cords=io.readFileMd(file,frame2_no,frame_no_pos=config.frame_no_pos)
-  print(physics.getCog(frame1_cords,atom_list=config.ring_atom_no_list))
-  print(physics.getCog(frame2_cords,atom_list=config.ring_atom_no_list))
   return _getRotation(frame1_cords,frame2_cords,part1=part1,part2=part2,type=type,method=method,part1_atom_list=part1_atom_list,part2_atom_list=part2_atom_list)
 
 def _getRotation(frame1_cords,frame2_cords,part1='ring',part2='track',type='absolute',method='rot_atomic_r_t_2',part1_atom_list=[],part2_atom_list=[]):
@@ -321,10 +321,11 @@ def rot_part_atomic_r_t_3(frame1_cords,frame2_cords,part='ring',atom_list=[]):
     cog2=physics.getCog(frame2_cords,atom_list=ring_atom_list)
     trans_axis[0]=cog2[0]-cog1[0]
     trans_axis[1]=cog2[1]-cog1[1]
-    trans_axis[2]=cog2[2]-cog1[2] 
-    nearest_atom_list=getNearestAtomList(frame1_cords,cog1,trans_axis,config.track_range)
+    trans_axis[2]=cog2[2]-cog1[2]
+    axis,origin=shift_origin.getAxisAndOrigin(frame1_cords,frame2_cords,process='rotation',axis=None,origin=None,system='molecular_machine')
+    nearest_atom_list=getNearestAtomList(frame1_cords,origin,axis,config.track_range)
     track_part_atom_list=list(filter(lambda x:x in track_atom_list,nearest_atom_list))
-    #print(config.track_range)
+    #print(f'distance from origin config.track_range)
     #print(track_part_atom_list)
     return rot_atomic_r_t_3(frame1_cords,frame2_cords,part='custom',atom_list=track_part_atom_list)
   else:
@@ -458,6 +459,8 @@ def getNearestAtomList(df,point,direction,distance):
     p[1]=atom_cords[1]-point[1]
     p[2]=atom_cords[2]-point[2]
     projection=vector.getDotProduct(direction,p)
+    print(f"distance from point {row['atom_no']} = {projection}")
+    print(f'point = {point}, atom cord = {atom_cords}')
     if isZero(p):
       continue
     if np.abs(round(projection,6))<=distance:

source/shift_origin.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from lib.basic_operations import physics,vector
 import config
 
@@ -9,66 +11,198 @@ def _shiftOrigin(cords,origin):
   new_cords['z']=new_cords['z']-origin[2]
   return new_cords
 
+def getAxisAndOrigin(frame1_cords,frame2_cords,process='rotation',axis=None,origin=None,system='molecular_machine'):
+  if system=='molecular_machine':
+    _atom_no_list=config.ring_atom_no_list
+  else:
+    _atom_no_list=frame1_cords['atom_no'].values
+
+  #print(_atom_no_list)
+  _axis=[0,0,0]
+  if (isinstance(axis,list) or isinstance(axis,np.ndarray)) and len(axis)==3:
+    _axis=axis
+  elif axis=='cog':
+      cog1=physics.getCog(frame1_cords,atom_list=_atom_no_list)
+      cog2=physics.getCog(frame2_cords,atom_list=_atom_no_list)
+      for i in range(3):
+        _axis[i]=cog2[i]-cog1[i]
+      if isZero(_axis):
+        if system=='molecular_machine':
+          _atom_no_list=config.track_atom_no_list
+          cog1=physics.getCog(frame1_cords,atom_list=_atom_no_list)
+          cog2=physics.getCog(frame2_cords,atom_list=_atom_no_list)
+          for i in range(3):
+            _axis[i]=cog2[i]-cog1[i]
+          if isZero(_axis):
+            _atom_no_list=config.ring_atom_no_list
+            cog1=physics.getCog(frame1_cords,atom_list=config.ring_atom_no_list)
+            cog2=physics.getCog(frame2_cords,atom_list=config.track_atom_no_list)
+            for i in range(3):
+              _axis[i]=cog2[i]-cog1[i]
+            if isZero(_axis):
+              print(f'_axis = {_axis}, Do not know other way to determine axis for {system}')
+              print('so returning the same coordinates')
+              return None
+        else:
+          print(f'_axis = {_axis}, Do not know other way to determine axis for {system}')
+          print('so returning the same coordinates')
+          return None
+  elif axis=='com':
+      com1=physics.getCom(frame1_cords,atom_list=_atom_no_list)
+      com2=physics.getCom(frame2_cords,atom_list=_atom_no_list)
+      for i in range(i):
+        _axis[i]=com2[i]-com1[i]
+      if isZero(_axis):
+        if system=='molecular_machine':
+          _atom_no_list=config.track_atom_no_list
+          com1=physics.getCom(frame1_cords,atom_list=_atom_no_list)
+          com2=physics.getCom(frame2_cords,atom_list=_atom_no_list)
+          for i in range(3):
+            _axis[i]=com2[i]-com1[i]
+          if isZero(_axis):
+            _atom_no_list=config.ring_atom_no_list
+            com1=physics.getCom(frame1_cords,atom_list=config.ring_atom_no_list)
+            com2=physics.getCom(frame2_cords,atom_list=config.track_atom_no_list)
+            for i in range(3):
+              _axis[i]=com2[i]-com1[i]
+            if isZero(_axis):
+              print(f'_axis = {_axis}, Do not know other way to determine axis for {system}')
+              print('so returning the same coordinates')
+              return None
+        else:
+          print(f'_axis = {_axis}, Do not know other way to determine axis for {system}')
+          print('so returning the same coordinates')
+          return None
+  elif type(axis)==type(None):
+    print('determining axis...')
+    if process=='rotation' or 'translation':
+      print('making ring cog as axis')
+      cog1=physics.getCog(frame1_cords,atom_list=_atom_no_list)
+      cog2=physics.getCog(frame2_cords,atom_list=_atom_no_list)
+      for i in range(3):
+        _axis[i]=cog2[i]-cog1[i]
+      if isZero(_axis):
+        print('changing rotation axis to track cog')
+        if system=='molecular_machine':
+          _atom_no_list=config.track_atom_no_list
+          cog1=physics.getCog(frame1_cords,atom_list=_atom_no_list)
+          cog2=physics.getCog(frame2_cords,atom_list=_atom_no_list)
+          for i in range(3):
+            _axis[i]=0#cog2[i]-cog1[i]
+          if isZero(_axis):
+            print('changing rotation axis to ring track cog')
+            _atom_no_list=config.ring_atom_no_list
+            cog1=physics.getCog(frame1_cords,atom_list=config.ring_atom_no_list)
+            cog2=physics.getCog(frame2_cords,atom_list=config.track_atom_no_list)
+            for i in range(3):
+              _axis[i]=cog2[i]-cog1[i]
+            if isZero(_axis):
+              print(f'_axis = {_axis}, Do not know other way to determine axis for {system}')
+              print('so returning the same coordinates')
+              return None
+        else:
+          print(f'_axis = {_axis}, Do not know other way to determine axis for {system}')
+          print('so returning the same coordinates')
+          return None
+    elif process=='translation':
+      com1=physics.getCom(frame1_cords,atom_list=_atom_no_list)
+      com2=physics.getCom(frame2_cords,atom_list=_atom_no_list)
+      for i in range(3):
+        _axis[i]=com2[i]-com1[i]
+      if isZero(_axis):
+        print('Changing to track com axis')
+        if system=='molecular_machine':
+          _atom_no_list=config.track_atom_no_list
+          com1=physics.getCom(frame1_cords,atom_list=_atom_no_list)
+          com2=physics.getCom(frame2_cords,atom_list=_atom_no_list)
+          for i in range(3):
+            _axis[i]=com2[i]-com1[i]
+          if isZero(_axis):
+            _atom_no_list=config.ring_atom_no_list
+            com1=physics.getCom(frame1_cords,atom_list=config.ring_atom_no_list)
+            com2=physics.getCom(frame2_cords,atom_list=config.track_atom_no_list)
+            for i in range(3):
+              _axis[i]=com2[i]-com1[i]
+            if isZero(_axis):
+              print(f'_axis = {_axis}, Do not know other way to determine axis for {system}')
+              print('so returning the same coordinates')
+              return None
+        else:
+          print(f'_axis = {_axis}, Do not know other way to determine axis for {system}')
+          print('so returning the same coordinates')
+          return None
+  else:
+    print(f'shiftOrigin: axis_argument = {axis}, cannont determine the axis')  
+
+  
+  com1=physics.getCom(frame1_cords,atom_list=_atom_no_list)
+  com2=physics.getCom(frame2_cords,atom_list=_atom_no_list)
+  sign= 1 if com2[0]-com1[0]>=0 else -1
+  for i in range(3):
+    _axis[i]*=sign
+  print(f'shift_origin axis={_axis}')
+
+
+  if system=='molecular_machine':
+    _atom_no_list=config.ring_atom_no_list
+  else:
+    _atom_no_list=frame1_cords['atom_no'].values
+
+  _origin=None
+  if (isinstance(origin,list) or isinstance(origin,np.ndarray)) and len(origin)==3:
+    _origin=origin
+  elif origin=='cog':
+      cog1=physics.getCog(frame1_cords,atom_list=_atom_no_list)
+      _origin=cog1
+  elif axis=='com':
+      com1=physics.getCom(frame1_cords,atom_list=_atom_no_list)
+      _origin=com1
+  elif type(origin)==type(None):
+    print('determining origin...')
+    if process=='rotation' or 'translation':
+      cog1=physics.getCog(frame1_cords,atom_list=_atom_no_list)
+      _origin=cog1
+    elif process=='translation':
+      com1=physics.getCom(frame1_cords,atom_list=_atom_no_list)
+      _origin=com1
+  else:
+    print(f'argument origin = {origin}, cannont determine the origin')
+  print(f'shift_origin origin={_origin}')
+
+  return _axis,_origin
+
 def isZero(l):
   for i in l:
     if round(i,6)!=0:
       return False
   return True
 
-def shiftOrigin(frame1_cords,frame2_cords,process='rotation',ref_axis_alignment=True): 
-  trans_axis=[0.0,0.0,0.0] 
-  com1=physics.getCom(frame1_cords,atom_list=config.ring_atom_no_list)
-  com2=physics.getCom(frame2_cords,atom_list=config.ring_atom_no_list)
-  sign= 1 if com2[0]-com1[0]>=0 else -1
-  #print(sign)
-  if process=='rotation':
-    cog1=physics.getCog(frame1_cords,atom_list=config.ring_atom_no_list)
-    cog2=physics.getCog(frame2_cords,atom_list=config.ring_atom_no_list)
-    trans_axis[0]=cog2[0]-cog1[0]
-    trans_axis[1]=cog2[1]-cog1[1]
-    trans_axis[2]=cog2[2]-cog1[2]
-    new_frame1_cords=_shiftOrigin(frame1_cords,cog1)
-    new_frame2_cords=_shiftOrigin(frame2_cords,cog2)
-  elif process=='translation':
-    cog1=physics.getCog(frame1_cords,atom_list=config.ring_atom_no_list)
-    cog2=physics.getCog(frame2_cords,atom_list=config.ring_atom_no_list)
-    trans_axis[0]=cog2[0]-cog1[0]
-    trans_axis[1]=cog2[1]-cog1[1]
-    trans_axis[2]=cog2[2]-cog1[2]
-    new_frame1_cords=_shiftOrigin(frame1_cords,cog1)
-    new_frame2_cords=_shiftOrigin(frame2_cords,cog1)
-    '''
-    com1=physics.getCom(frame1_cords,atom_list=config.ring_atom_no_list)
-    com2=physics.getCom(frame2_cords,atom_list=config.ring_atom_no_list)
-    trans_axis[0]=com2[0]-com1[0]
-    trans_axis[1]=com2[1]-com1[1]
-    trans_axis[2]=com2[2]-com1[2]
-    new_frame1_cords=_shiftOrigin(frame1_cords,com1)
-    new_frame2_cords=_shiftOrigin(frame2_cords,com1)
-    '''
-  if isZero(trans_axis):
-    print('COG1/COM1 == COG2/COM2, cannont determine rotation axis')
-    print('Changing rotation/translation axis')
-    com1=physics.getCom(frame1_cords,atom_list=config.track_atom_no_list)
-    com2=physics.getCom(frame2_cords,atom_list=config.track_atom_no_list)
-    trans_axis[0]=com2[0]-com1[0]
-    trans_axis[1]=com2[1]-com1[1]
-    trans_axis[2]=com2[2]-com1[2]
-  sign= 1 if com2[0]-com1[0]>=0 else -1
- 
-  for i in range(3):
-    trans_axis[i]*=sign
+def shiftOrigin(frame1_cords,frame2_cords,process='rotation',axis=None,origin=None,system='molecular_machine',ref_axis_alignment=True): 
+  '''
+  axis: is required for rotation as well as translation
+        if axis is not given then it assumed to be COG/COM axis
+
+  origin: is required only for rotation
+          if origin is not given it is assumed to be COG/COM of the first frame
+  '''
+  _axis,_origin=getAxisAndOrigin(frame1_cords,frame2_cords,process='rotation',axis=None,origin=None,system='molecular_machine')
+  if type(_axis)==type(None):
+    return (frame1_cords,frame2_cords)
+
+  assert not isZero(_axis),'shiftOrigin: _axis is zero even after this, what the ..'
+
+  new_frame1_cords=_shiftOrigin(frame1_cords,_origin)
+  new_frame2_cords=_shiftOrigin(frame2_cords,_origin)  
   if config.axis=='x':
     ax=[1,0,0]
   elif config.axis=='y':
     ax=[0,1,0]
   elif config.axis=='z':
     ax=[0,0,1]
-  axis=vector.getCrossProduct(trans_axis,ax)
-  theta=vector.getAngleR(trans_axis,ax)
-  new_frame1_cords=physics.rotateAlongAxis(new_frame1_cords,axis,theta)
-  new_frame2_cords=physics.rotateAlongAxis(new_frame2_cords,axis,theta)
-
+  temp_axis=vector.getCrossProduct(_axis,ax)
+  theta=vector.getAngleR(_axis,ax)
+  new_frame1_cords=physics.rotateAlongAxis(new_frame1_cords,temp_axis,theta)
+  new_frame2_cords=physics.rotateAlongAxis(new_frame2_cords,temp_axis,theta)
 
   #REFERENCE AXIS ALIGNMENT
   if ref_axis_alignment:
@@ -82,10 +216,10 @@ def shiftOrigin(frame1_cords,frame2_cords,process='rotation',ref_axis_alignment=
     elif config.axis=='z':
       ref_axis=[1,1,0]
       whole_frame_com[2]=0
-    axis=vector.getUnitVec(vector.getCrossProduct(whole_frame_com,ref_axis))
-    assert not isZero(axis),'ShiftOrigin:Error during reference axis alignment, cannont determine rotation axis'
+    temp_axis=vector.getUnitVec(vector.getCrossProduct(whole_frame_com,ref_axis))
+    assert not isZero(temp_axis),'ShiftOrigin:Error during reference axis alignment, cannont determine rotation axis'
     theta=vector.getAngleR(whole_frame_com,ref_axis)
-    new_frame1_cords=physics.rotateAlongAxis(new_frame1_cords,axis,theta)
-    new_frame2_cords=physics.rotateAlongAxis(new_frame2_cords,axis,theta)
+    new_frame1_cords=physics.rotateAlongAxis(new_frame1_cords,temp_axis,theta)
+    new_frame2_cords=physics.rotateAlongAxis(new_frame2_cords,temp_axis,theta)
   
   return (new_frame1_cords,new_frame2_cords)

verification/assessment_of_methods.py
@@ -28,11 +28,11 @@ def _init(method,system):
     subprocess.run(['mkdir',f'assessment_{method}'],cwd='output')
     subprocess.run(['mkdir','artificial_test_system','semi_real_test_system'],cwd=f'output/assessment_{method}')
     subprocess.run(['mkdir','ring','track'],cwd=f'output/assessment_{method}/artificial_test_system')
-    subprocess.run(['mkdir','ring/translation_without_rotation_test','ring/translation_with_single_axis_rotation_test','ring/translation_with_random_triple_axis_rotation_test'],cwd=f'output/assessment_{method}/artificial_test_system')
-    subprocess.run(['mkdir','track/translation_without_rotation_test','track/translation_with_single_axis_rotation_test','track/translation_with_random_triple_axis_rotation_test'],cwd=f'output/assessment_{method}/artificial_test_system')
+    subprocess.run(['mkdir','ring/translation_without_rotation_test','ring/translation_with_single_axis_rotation_1d_test','ring/translation_with_single_axis_rotation_3d_test','ring/translation_with_random_triple_axis_rotation_test'],cwd=f'output/assessment_{method}/artificial_test_system')
+    subprocess.run(['mkdir','track/translation_without_rotation_test','track/translation_with_single_axis_rotation_1d_test','track/translation_with_single_axis_rotation_3d_test','track/translation_with_random_triple_axis_rotation_test'],cwd=f'output/assessment_{method}/artificial_test_system')
     subprocess.run(['mkdir','ring','track'],cwd=f'output/assessment_{method}/semi_real_test_system')
-    subprocess.run(['mkdir','ring/translation_without_rotation_test','ring/translation_with_single_axis_rotation_test','ring/translation_with_random_triple_axis_rotation_test'],cwd=f'output/assessment_{method}/semi_real_test_system')
-    subprocess.run(['mkdir','track/translation_without_rotation_test','track/translation_with_single_axis_rotation_test','track/translation_with_random_triple_axis_rotation_test'],cwd=f'output/assessment_{method}/semi_real_test_system')
+    subprocess.run(['mkdir','ring/translation_without_rotation_test','ring/translation_with_single_axis_rotation_1d_test','ring/translation_with_single_axis_rotation_3d_test','ring/translation_with_random_triple_axis_rotation_test'],cwd=f'output/assessment_{method}/semi_real_test_system')
+    subprocess.run(['mkdir','track/translation_without_rotation_test','track/translation_with_single_axis_rotation_1d_test','track/translation_with_single_axis_rotation_3d_test','track/translation_with_random_triple_axis_rotation_test'],cwd=f'output/assessment_{method}/semi_real_test_system')
   else:
     print(f'{method} does not exist, not creating directories')
   if system=='artificial':
@@ -57,10 +57,11 @@ def assessRotationMethodSingleAxis(method='rot_hybrid_3',step_size=10,parts=['ri
           x=[]
           y=[]
           for theta in range(-90,91,step_size):
+            print('*'*50)
             rpy[axis]=theta
             if part=='ring':
               if system=='artificial':
-                make_test_systems.ringTrackTwoFramesNonIdealArtificial(ring_rpy=rpy,track_rpy=zero_rpy,ring_translation=_ring_translation,track_translation=track_translation)
+                make_test_systems.ringTrackTwoFramesNonIdealArtificial(ring_rpy=rpy,track_rpy=zero_rpy,ring_translation=ring_translation,track_translation=track_translation)
               elif system=='semi_real':
                 make_test_systems.ringTrackTwoFramesSemiReal(ring_rpy=rpy,track_rpy=zero_rpy,ring_translation=ring_translation,track_translation=track_translation)
             elif part=='track':
@@ -323,9 +324,41 @@ def assessTranslationMethodWithoutRotation(method='',step_size=10,parts=['ring',
       print(f'{distance},{part} Absolute Translation = {_translation},{method},translation_without_rotation,{system}')
     plot4(x,y,method=method,part=part,assessment_type='translation_without_rotation',system=system,show_plot=show_plot)
 
-def assessTranslationMethodSingleAxis3d(method='',rotation_axis=1,step_size=120,parts=['ring','track'],system='',show_plot=True):
+def assessTranslationMethodWithSingleAxisRotation1d(method='',rotation_axis=1,theta=60,step_size=120,parts=['ring','track'],system='',show_plot=True):
   distance_range=np.arange(-10,11,step_size)
-  theta_range=range(-50,51,5)
+  frame1_no=0
+  frame2_no=1
+  zero_rpy=[0,0,0]
+  rpy=[0,0,0]
+  rpy[rotation_axis]=theta
+  for part in parts:
+    x=[]
+    y=[]
+    for distance in distance_range:
+      if part=='ring':
+        if system=='artificial':
+          make_test_systems.ringTrackTwoFramesNonIdealArtificial(ring_translation=distance,track_translation=0,ring_rpy=rpy,track_rpy=zero_rpy)
+        elif system=='semi_real':
+          make_test_systems.ringTrackTwoFramesSemiReal(ring_translation=distance,track_translation=0,ring_rpy=rpy,track_rpy=zero_rpy)
+      elif part=='track':
+        if system=='artificial':
+          make_test_systems.ringTrackTwoFramesNonIdealArtificial(ring_translation=0,track_translation=distance,ring_rpy=zero_rpy,track_rpy=rpy)
+        elif system=='semi_real':
+          make_test_systems.ringTrackTwoFramesSemiReal(ring_translation=0,track_translation=distance,ring_rpy=zero_rpy,track_rpy=rpy)
+      if system=='artificial':
+        file_path='test_systems/ring_track_two_frames_non_ideal_artificial_system.xyz'
+      elif system=='semi_real':
+        file_path='test_systems/ring_track_two_frames_semi_real_system.xyz'
+      with open(file_path,'r') as file:
+        _translation=translation.getTranslation(file,frame1_no,frame2_no,part1=part,part2='track',type='absolute',method=method,unit='A')
+      x.append(distance)
+      y.append(_translation)
+      print(f'{distance},{part} Absolute Translation = {_translation},{method},translation_with_single_axis_rotation_1d,{system}')
+    plot4(x,y,method=method,part=part,assessment_type='translation_with_single_axis_rotation_1d',system=system,show_plot=show_plot)
+
+def assessTranslationMethodWithSingleAxisRotation3d(method='',rotation_axis=1,step_size=120,parts=['ring','track'],system='',show_plot=True):
+  distance_range=np.arange(-10,11,step_size)
+  theta_range=range(-90,91,10)
   frame1_no=0
   frame2_no=1
   zero_rpy=[0,0,0]
@@ -362,12 +395,12 @@ def assessTranslationMethodSingleAxis3d(method='',rotation_axis=1,step_size=120,
         axis_theta_list.append(theta)
         z_expected.append(distance)
         z_predicted.append(_translation)
-        print(f'{distance},{rpy},{part} Absolute Translation = {_translation},{method},translation_with_single_axis_rotation,3d,{rotation_axis},{system}')
+        print(f'{distance},{rpy},{part} Absolute Translation = {_translation},{method},translation_with_single_axis_rotation_3d,{rotation_axis},{system}')
       X.append(x)
       Y.append(axis_theta_list)
       Z_expected.append(z_expected)
       Z_predicted.append(z_predicted)
-    plot5(X,Y,np.array(Z_expected),np.array(Z_predicted),method=method,part=part,rotation_axis=rotation_axis,assessment_type='translation_with_single_axis_rotation',system=system,show_plot=show_plot)
+    plot5(X,Y,np.array(Z_expected),np.array(Z_predicted),method=method,part=part,rotation_axis=rotation_axis,assessment_type='translation_with_single_axis_rotation_3d',system=system,show_plot=show_plot)
 
 def assessTranslationMethodWithRandomTripleAxisRotation1d(method='',step_size=10,parts=['ring','track'],system='',show_plot=True):
   distance_range=np.arange(-10,11,step_size)
@@ -569,13 +602,13 @@ def plotConeAssessment(x,y,method='',part='ring',axis=0,assessment_type='',syste
     axis='z'
   plt.figure(figsize=(16,8))
   plt.rcParams.update({'font.size': 15})
-  plt.plot(x,y,'r',label='Predicted')
+  plt.plot(x,y,'r',label='Error')
   if axis=='x':
     plt.plot(x,x,'b',label='Expected')
   else:
     zero=np.zeros(len(x))
     plt.plot(x,zero,'b',label='Expected')
-  title=method.upper()+f'{system},'+part+','+axis+','+assessment_type
+  title=method.upper()+f'({system},'+part+','+axis+','+assessment_type
   plt.title(title)
   plt.xlabel(f'{axis} Rotation(D)')
   plt.ylabel('Predicted/Expected x Rotation (D)')
@@ -657,19 +690,20 @@ def plotRKE(x,y_predicted,y_expected,method='',part='',axis='',assessment_type='
   if show_plot:
     plt.show()
 
-'''
+
+
 #ROTATION
-system_list=['semi_real']
+system_list=['artificial']
 method_list=['rot_part_atomic_r_t_3']
 step_size=5
-parts=['ring','track']
-translation_list=[3.14,-3.14]
+parts=['track']
+translation_list=[0,3.14,-3.14]
 show_plot=True
 for system in system_list:
   for method in method_list:
     _init(method,system)
-    coneAssessment(method=method,system=system,parts=parts,step_size=step_size)
-    #assessRotationMethodSingleAxis(method=method,step_size=step_size,parts=parts,system=system,translation_list=translation_list,show_plot=show_plot)
+    #coneAssessment(method=method,system=system,parts=parts,step_size=step_size)
+    assessRotationMethodSingleAxis(method=method,step_size=step_size,parts=parts,system=system,translation_list=translation_list,show_plot=show_plot)
     #coneAssessment(system=system,parts=parts,step_size=step_size)
     #assessRotationMethodDoubleAxis2d(method=method,step_size=step_size,rotation_axis=0,constant_axis=1,constant_theta=45,system=system,show_plot=show_plot)
     #assessRotationMethodDoubleAxis2d(method=method,step_size=step_size,rotation_axis=0,constant_axis=1,constant_theta=-45,system=system,show_plot=show_plot)
@@ -682,7 +716,7 @@ for system in system_list:
 
 
 #TRANSLATION
-system_list=['semi_real']
+system_list=['artificial']
 method_list=['trans_com_3']
 step_size=0.5
 parts=['ring','track']
@@ -690,14 +724,15 @@ show_plot=True
 for system in system_list:
   for method in method_list:
     _init(method,system)
-    assessTranslationMethodWithoutRotation(method=method,step_size=step_size,parts=parts,system=system,show_plot=show_plot)
-    assessTranslationMethodSingleAxis3d(method=method,rotation_axis=0,step_size=step_size,parts=parts,system=system,show_plot=show_plot)
-    assessTranslationMethodSingleAxis3d(method=method,rotation_axis=1,step_size=step_size,parts=parts,system=system,show_plot=show_plot)
-    assessTranslationMethodSingleAxis3d(method=method,rotation_axis=2,step_size=step_size,parts=parts,system=system,show_plot=show_plot)
-    assessTranslationMethodWithRandomTripleAxisRotation1d(method=method,step_size=step_size,parts=parts,system=system,show_plot=show_plot)
-'''
+    #assessTranslationMethodWithoutRotation(method=method,step_size=step_size,parts=parts,system=system,show_plot=show_plot)
+    assessTranslationMethodWithSingleAxisRotation1d(method=method,rotation_axis=0,theta=90,step_size=step_size,parts=parts,system=system,show_plot=show_plot)
+    #assessTranslationMethodSingleAxisWithRotation3d(method=method,rotation_axis=0,step_size=step_size,parts=parts,system=system,show_plot=show_plot)
+    #assessTranslationMethodSingleAxisWithRotation3d(method=method,rotation_axis=1,step_size=step_size,parts=parts,system=system,show_plot=show_plot)
+    #assessTranslationMethodSingleAxisWithRotation3d(method=method,rotation_axis=2,step_size=step_size,parts=parts,system=system,show_plot=show_plot)
+    #assessTranslationMethodWithRandomTripleAxisRotation1d(method=method,step_size=step_size,parts=parts,system=system,show_plot=show_plot)
 
-assessRKEMethodSingleAxis(method='energy_rot_part_atomic_r_t_3',system='artificial',parts=['ring','track'],step_size=5,show_plot=True)
+
+#assessRKEMethodSingleAxis(method='energy_rot_part_atomic_r_t_3',system='artificial',parts=['ring','track'],step_size=5,show_plot=True)
 
 #coneAssessment()
 #_init('rot_part_atomic_r_t_3','artificial')