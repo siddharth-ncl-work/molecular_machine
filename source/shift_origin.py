68c3a2c72e87355e4f03b8aeb842ffa948578bde
ss-ghule <ss.ghule@ncl.res.in>
2020-07-08 13:39:49 +0530

    [DRAWBACK]Try to solve previous issue: change COG-->COM -issue: cannot determine axis for only track rotation -use COM instead of COG during determination of axis using track when ring does not move -did not work -results are very much similar to the results obtained using COG -it might be because COG and COM are very close for the track -all plots and data is present at the following location: -/home/vanka/siddharth/molecular_machine_project/molecular_machine/verification/output/assessment_rot_part_atomic_r_t_3/artificial_test_system/track/single_axis_test
    NOTE:
    -so it is concluded that currently there is no method to determine rotation axis when ring and track does not translate
    -this issue will not be investigated further
    -this is issue is the part of current alorithm and can considered as one of the drawbacks

--- source/shift_origin.py
+++ source/shift_origin.py
@@ -82,13 +82,20 @@ def getAxisAndOrigin(frame1_cords,frame2_cords,process='rotation',axis=None,orig
       for i in range(3):
         _axis[i]=cog2[i]-cog1[i]
       if isZero(_axis):
-        print('changing rotation axis to track cog')
+        print('changing rotation axis to track com')
         if system_type=='molecular_machine':
           _atom_no_list=config.track_atom_no_list
+          '''
           cog1=physics.getCog(frame1_cords,atom_list=_atom_no_list)
           cog2=physics.getCog(frame2_cords,atom_list=_atom_no_list)
           for i in range(3):
             _axis[i]=cog2[i]-cog1[i]
+          '''
+          com1=physics.getCom(frame1_cords,atom_list=_atom_no_list)
+          com2=physics.getCom(frame2_cords,atom_list=_atom_no_list)
+          for i in range(3):
+            _axis[i]=com2[i]-com1[i]
+
           if isZero(_axis):
             print('changing rotation axis to ring track cog')
             _atom_no_list=config.ring_atom_no_list
